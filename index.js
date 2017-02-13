93ca51d2726f3b286872c7e79fc32a3f542b49c8
drinkinginthesky <466952688@qq.com>
2017-02-13 16:31:59 +0800

    code format & fix

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+'use strict';
+
 var config = require('./config');
 var fs = require('fs');
 var async = require('async');
 
 var formate = function (data) {
-    return data.replace(/\example/g, config.example)
-                .replace(/\Example/g, config.Example)
-                .replace(/\chineseDesc/g, config.chineseDesc)
-                .replace(/\fileName/g, config.fileName);
+    return data.replace(/example/g, config.example)
+        .replace(/Example/g, config.Example)
+        .replace(/chineseDesc/g, config.chineseDesc)
+        .replace(/fileName/g, config.fileName);
 };
 
 var writeFile = function (path, data, callback) {
-   fs.writeFile(path, data, 'utf8', function (err) {
+    fs.writeFile(path, data, 'utf8', function (err) {
         if (!!err) {
             callback(err);
             return;
@@ -35,7 +37,7 @@ var routefn = function (callback) {
     var route = fs.createReadStream('./source/route.js');
     var data = '';
     var resultPath = './result/' + config.fileName + '.server.route.js';
-    route.on("data", function (trunk){
+    route.on("data", function (trunk) {
         data += trunk;
     });
     route.on("end", function () {
@@ -45,9 +47,12 @@ var routefn = function (callback) {
 };
 
 
-async.series([
-    controllerfn,
-    routefn
-    ], function (err, result) {
+async.series(
+    [
+        controllerfn,
+        routefn
+    ],
+    function (err, result) {
         console.log('success!');
-    });
+    }
+);
